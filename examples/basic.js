a0cd6a77dc9cfaafe4276b9e0ef4ff01de7c9a37
Cache Hamm <cache.hamm@welltok.com>
2016-04-12 07:15:37 -0600

    simplify example and improve commenting

--- examples/basic.js
+++ examples/basic.js
@@ -1,18 +1,27 @@
 'use strict'
 
+/*
+ * This is a basic example that demonstrates a simple run() of the rules engine on
+ * two different sets of data.
+ *
+ * In this example, Facts are passed to run() as constants known at runtime.  For a more
+ * complex example demonstrating computed facts, see the computed-facts example.
+ *
+ * Usage:
+ *   node ./examples/basic.js
+ */
+
 require('babel-polyfill')
-var colors = require('colors')
+require('colors')
 var Engine = require('../dist').Engine
 
 /**
  * Setup a new engine
- * @type {Engine}
  */
 var engine = new Engine()
 
 /**
  * Add an 'income' rule that will emit for incomes over 20,000
- * @type {Object}
  */
 var incomeRule = {
   conditions: {
@@ -34,7 +43,6 @@ engine.addRule(incomeRule)
 
 /**
  * Add an 'education' rule that will emit for high-school educations
- * @type {Object}
  */
 var educationRule = {
   conditions: {
@@ -53,36 +61,32 @@ engine.addRule(educationRule)
 /**
  * Register listeners with the engine for rule success and failure
  */
-let promises = []
-engine.on('success', function (event, almanac) {
-  promises.push(almanac.factValue('username').then(function (username) {
-    console.log(username + ' DID '.green + 'meet conditions for the ' + event.type.underline + ' rule')
-  }))
-})
-engine.on('failure', function (rule, almanac) {
-  promises.push(almanac.factValue('username').then(function (username) {
-    console.log(username + ' did ' + 'NOT'.red + ' meet conditions for the ' + rule.event.type.underline + ' rule')
-  }))
-})
+var facts
+engine
+  .on('success', function (event, almanac) {
+    console.log(facts.username + ' DID '.green + 'meet conditions for the ' + event.type.underline + ' rule. params: ' + JSON.stringify(event.params))
+  })
+  .on('failure', function (rule, almanac) {
+    console.log(facts.username + ' did ' + 'NOT'.red + ' meet conditions for the ' + rule.event.type.underline + ' rule.')
+  })
 
 /**
  * Each run() of the engine executes on an independent set of facts.  We'll run twice, once
  * for each user we want to evaluate
  */
-var washingtonData = {
+facts = {
   income: 30000,
   education: 'college',
   username: 'washington'
 }
-promises.push(engine.run(washingtonData))
-
-var jeffersonData = {
-  income: 15000,
-  education: 'high-school',
-  username: 'jefferson'
-}
-promises.push(engine.run(jeffersonData))
-
-Promise.all(promises).catch(function (err) {
-  console.log(err)
-})
+engine
+  .run(facts)  // first run, using washington's facts
+  .then(function () {
+    facts = {
+      income: 15000,
+      education: 'high-school',
+      username: 'jefferson'
+    }
+    return engine.run(facts) // second run, using jefferson's facts; facts & evaluation are independent of the first run
+  })
+  .catch(console.log)
