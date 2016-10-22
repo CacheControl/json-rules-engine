cc833956c5e144a3a58aa662d26b2e2e3c18c023
Cache Hamm <cache.hamm@shutterstock.com>
2016-10-22 14:25:35 -0600

    convert array operators to use indexOf instead of includes for backwards compatibility

--- src/engine-default-operators.js
+++ src/engine-default-operators.js
@@ -5,11 +5,11 @@ import Operator from './operator'
 let Operators = []
 Operators.push(new Operator('equal', (a, b) => a === b))
 Operators.push(new Operator('notEqual', (a, b) => a !== b))
-Operators.push(new Operator('in', (a, b) => b.includes(a)))
-Operators.push(new Operator('notIn', (a, b) => !b.includes(a)))
+Operators.push(new Operator('in', (a, b) => b.indexOf(a) > -1))
+Operators.push(new Operator('notIn', (a, b) => b.indexOf(a) === -1))
 
-Operators.push(new Operator('contains', (a, b) => a.includes(b), Array.isArray))
-Operators.push(new Operator('doesNotContain', (a, b) => !a.includes(b), Array.isArray))
+Operators.push(new Operator('contains', (a, b) => a.indexOf(b) > -1, Array.isArray))
+Operators.push(new Operator('doesNotContain', (a, b) => a.indexOf(b) === -1, Array.isArray))
 
 function numberValidator (factValue) {
   return Number.parseFloat(factValue).toString() !== 'NaN'
