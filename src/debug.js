31d9355bd85f3d63cf4ef65c082d07c53ed770e1
Cache Hamm <chamm@shutterstock.com>
2019-07-14 10:42:34 -0600

    fix DEBUG=json-rules-engine in browsers

--- src/debug.js
+++ src/debug.js
@@ -1,5 +1,6 @@
 export default function debug (message) {
-  if (typeof process !== 'undefined' && process.env && process.env.DEBUG && process.env.DEBUG.match(/json-rules-engine/)) {
+  if ((typeof process !== 'undefined' && process.env && process.env.DEBUG && process.env.DEBUG.match(/json-rules-engine/)) ||
+      (typeof window !== 'undefined' && window.DEBUG && window.DEBUG.match(/json-rules-engine/))) {
     console.log(message)
   }
 }
