6653754c40806a66b1e87e189ba42821e4acfea7
Cache Hamm <chamm@shutterstock.com>
2019-11-29 15:35:06 -0700

    Add test for path + array operators

--- test/engine-fact.test.js
+++ test/engine-fact.test.js
@@ -168,6 +168,16 @@ describe('Engine: fact evaluation', () => {
       expect(successSpy).to.not.have.been.called()
     })
 
+    it('can extract an array, allowing it to be used in concert with array operators', async () => {
+      const complexCondition = conditions()
+      complexCondition.any[0].path = '$.address.occupantHistory[*].year'
+      complexCondition.any[0].value = 2011
+      complexCondition.any[0].operator = 'contains'
+      setup(complexCondition)
+      await engine.run()
+      expect(successSpy).to.have.been.calledWith(event)
+    })
+
     context('complex paths', () => {
       it('correctly interprets "path" when dynamic facts return objects', async () => {
         const complexCondition = conditions()
