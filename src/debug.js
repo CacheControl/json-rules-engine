1ce4bbef32a687f20555c5c434b57c29a9c76907
hemang-thakkar <59508898+hemang-thakkar@users.noreply.github.com>
2020-01-16 16:44:58 -0500

    Update debug.js
    Wrapping if statement in try/catch block

--- src/debug.js
+++ src/debug.js
@@ -1,6 +1,10 @@
 export default function debug (message) {
-  if ((typeof process !== 'undefined' && process.env && process.env.DEBUG && process.env.DEBUG.match(/json-rules-engine/)) ||
-      (typeof window !== 'undefined' && window.localStorage && window.localStorage.debug && window.localStorage.debug.match(/json-rules-engine/))) {
-    console.log(message)
+  try {
+    if ((typeof process !== 'undefined' && process.env && process.env.DEBUG && process.env.DEBUG.match(/json-rules-engine/)) ||
+        (typeof window !== 'undefined' && window.localStorage && window.localStorage.debug && window.localStorage.debug.match(/json-rules-engine/))) {
+      console.log(message)
+    }
+  } catch(ex) {
+    // Do nothing
   }
 }
