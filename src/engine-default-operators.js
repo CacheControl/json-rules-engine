c8cddf5fda9e32432648bfb31c13afe95671bd98
Itay Ganor <ganor.itay@gmail.com>
2020-02-05 21:59:36 +0200

    Made contains and doesNotContain support both strings and arrays

--- src/engine-default-operators.js
+++ src/engine-default-operators.js
@@ -8,8 +8,11 @@ Operators.push(new Operator('notEqual', (a, b) => a !== b))
 Operators.push(new Operator('in', (a, b) => b.indexOf(a) > -1))
 Operators.push(new Operator('notIn', (a, b) => b.indexOf(a) === -1))
 
-Operators.push(new Operator('contains', (a, b) => a.indexOf(b) > -1, Array.isArray))
-Operators.push(new Operator('doesNotContain', (a, b) => a.indexOf(b) === -1, Array.isArray))
+function arrayOfStringValidator (factValue) {
+  return Array.isArray(factValue) || (typeof factValue === 'string' || factValue instanceof String)
+}
+Operators.push(new Operator('contains', (a, b) => a.indexOf(b) > -1, arrayOfStringValidator))
+Operators.push(new Operator('doesNotContain', (a, b) => a.indexOf(b) === -1, arrayOfStringValidator))
 
 function numberValidator (factValue) {
   return Number.parseFloat(factValue).toString() !== 'NaN'
