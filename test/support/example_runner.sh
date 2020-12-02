763c35993759d6868c5e80e7faa50d248ac10896
Cache Hamm <chamm@shutterstock.com>
2020-12-02 07:23:37 -0700

    Add example runner script

---
+++ test/support/example_runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# npm links the examples directory to the local copy of json-rules-engine
+# and runs all examples. This can be used to test a release candidate version
+# against the examples as an extra compatiblity test
+#
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $THIS_DIR/../.. # project root
+npm link
+cd $THIS_DIR/../../examples # examples directory
+npm link json-rules-engine
+for i in *.js; do node $i; done;
