d248eb5ff20048fceca5335500c71b7e64139cd9
akmjenkins <akmjenkins@gmail.com>
2021-09-02 00:58:45 -0300

    Allows interpolation of stuff other than just strings

--- src/interpolate.js
+++ src/interpolate.js
@@ -2,12 +2,26 @@ export const defaultInterpolation = /\{\{\s*(.+?)\s*\}\}/g
 
 export const needsInterpolation = (rule,regexp) => regexp.test(rule.toJSON(true));
 
+const interpolate = (subject = '', params = {}, regexp, resolver) => {
+  let shouldReplaceFull, found;
+
+  const replaced = subject.replace(regexp, (full, matched) => {
+    shouldReplaceFull = full === subject;
+    found = resolver(params, matched);
+    return shouldReplaceFull ? '' : found;
+  });
+
+  return shouldReplaceFull ? found : replaced;
+};
+
+
 export const interpolateDeep = (o, params, regexp, resolver) => {
   if (!o || typeof o === 'number' || typeof o === 'boolean') return o;
 
-  if (typeof o === 'string') return o.replace(regexp, (_, matched) => resolver(params,matched))
+  if (typeof o === 'string') return interpolate(o,params,regexp,resolver)
 
   if (Array.isArray(o)) return o.map(t => interpolateDeep(t, params, regexp, resolver));
 
   return Object.entries(o).reduce((acc, [k, v]) => ({...acc,[k]: interpolateDeep(v, params, regexp, resolver)}),{});
 };
+
